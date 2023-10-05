649784c5d2a0a7942dd2cb546c5e8f9d97c43a28
ZIELIK <41583193+zielik-dev@users.noreply.github.com>
2023-10-05 22:00:21 +0100

    Update ConnectionStrings.cs

--- Order Pool Report - 500 & 520/Infrastructure/ConnectionStrings.cs
+++ Order Pool Report - 500 & 520/Infrastructure/ConnectionStrings.cs
@@ -6,7 +6,7 @@
         public static string ecoSystemDbConnection = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Config\EcoSystemDatabaseConfig.xml";
         public static string ecoSystemEmailDistributionConfig = @"\\XXX\LocalData$\XXXn\.NET Core Solution - Production Env\.Config\EcoSystemEmailDistributionConfig.xml";
         public static string sftpB2Bconfig = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Config\RedPrairie.config.xml";
-        public static string smtpConfigFile = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Config\Gxo.smtp.server.config.xml";
+        public static string smtpConfigFile = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Config\Smtp.server.config.xml";
 
         public static string archiveDir = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Archive\Order Pool Report - 500 & 520\";
         public static string queryFile = "Order_Pool_500_520_Query.sql";
