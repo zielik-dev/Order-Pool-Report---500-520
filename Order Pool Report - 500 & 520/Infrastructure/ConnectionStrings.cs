4da32dd1418b17244f0bc78b2de8aa6fa6d3ee36
ZIELIK <41583193+zielik-dev@users.noreply.github.com>
2023-10-05 21:43:53 +0100

    Update ConnectionStrings.cs

--- Order Pool Report - 500 & 520/Infrastructure/ConnectionStrings.cs
+++ Order Pool Report - 500 & 520/Infrastructure/ConnectionStrings.cs
@@ -2,16 +2,16 @@
 {
     public class ConnectionStrings
     {
-        public static string ecoSystemGeneralConfig = @"\\WELDS7033\LocalData$\Virgin Automation\.NET Core Solution - Production Env\.Config\EcoSystemGeneralConfig.xml";
-        public static string ecoSystemDbConnection = @"\\WELDS7033\LocalData$\Virgin Automation\.NET Core Solution - Production Env\.Config\EcoSystemDatabaseConfig.xml";
-        public static string ecoSystemEmailDistributionConfig = @"\\WELDS7033\LocalData$\Virgin Automation\.NET Core Solution - Production Env\.Config\EcoSystemEmailDistributionConfig.xml";
-        public static string sftpB2Bconfig = @"\\WELDS7033\LocalData$\Virgin Automation\.NET Core Solution - Production Env\.Config\RedPrairie.config.xml";
-        public static string smtpConfigFile = @"\\WELDS7033\LocalData$\Virgin Automation\.NET Core Solution - Production Env\.Config\Gxo.smtp.server.config.xml";
+        public static string ecoSystemGeneralConfig = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Config\EcoSystemGeneralConfig.xml";
+        public static string ecoSystemDbConnection = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Config\EcoSystemDatabaseConfig.xml";
+        public static string ecoSystemEmailDistributionConfig = @"\\XXX\LocalData$\XXXn\.NET Core Solution - Production Env\.Config\EcoSystemEmailDistributionConfig.xml";
+        public static string sftpB2Bconfig = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Config\RedPrairie.config.xml";
+        public static string smtpConfigFile = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Config\Gxo.smtp.server.config.xml";
 
-        public static string archiveDir = @"\\WELDS7033\LocalData$\Virgin Automation\.NET Core Solution - Production Env\.Archive\Order Pool Report - 500 & 520\";
+        public static string archiveDir = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Archive\Order Pool Report - 500 & 520\";
         public static string queryFile = "Order_Pool_500_520_Query.sql";
 
-        public static string srcTemplateDir = @"\\welds7033\LocalData$\Virgin Automation\.NET Core Solution - Production Env\.Templates\";
+        public static string srcTemplateDir = @"\\XXX\LocalData$\XXX\.NET Core Solution - Production Env\.Templates\";
         public static string srcTemplateFile = "Order Pool Report - 500 & 520.xlsm";
         public static string srcUnPlanned = "UnPlanned";
         public static string srcPlanned = "Planned";
